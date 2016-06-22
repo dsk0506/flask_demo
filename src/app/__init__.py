fcdc24069bd77c29b24d3692c11d967f0815bcc5
dingshoukun <dingshoukun@quancheng-ec.com>
2016-06-22 14:01:20 +0800

    配置调整

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,6 +1,17 @@
-__author__ = 'dsk'
+# coding: UTF-8
 from flask import Flask
+from config import load_config  # 绝对导入
 
-app = Flask(__name__)
-client = app.test_client()
+
+def create_app():
+    """创建Flask app"""
+    app = Flask(__name__)
+    # Load config
+    config = load_config()
+    app.config.from_object(config)
+
+    return app
+
+
+app = create_app()
 from app.modules.user.controllers import *
--- src/app/modules/user/controllers/user.py
+++ src/app/modules/user/controllers/user.py
@@ -1,4 +1,4 @@
-from app import app
+from app import  app
 from flask import request, make_response
 from flask import jsonify
 
